7781a61a7d015019fd3dcfda8f975a1c4ee7fb65
Tuomas-Talkio <122274744+Tuomas-Talkio@users.noreply.github.com>
2025-10-20 15:11:19 +0300

    koodin uudelleen järjestys
    järjestetään koodi loogisempaan järjestykseen. Luodaan kaikki muuttujat alussa ja sen jälkeen vasta tulostetaan stringiä ja muuttujia

--- src/Tulostin.java
+++ src/Tulostin.java
@@ -1,42 +1,35 @@
 public class Tulostin {
     public static void main(String[] args) throws Exception {
 
-        //tulostetaan tervehdys
-        System.out.println("Hei olen tulostin ohjelma");
-        System.out.print("Ohjelman tekijä: ");
-
-        //luodaan String muuttuja ja tulostetaan se
+        //luodaan tekija muuttuja
         String tekija = "Tuomas Talkio";
 
-        System.out.println(tekija);
-        
-        //Luodaan kaksi int muuttujaa
+        //Luodaan kaksi luku muuttujaa
         double luku1 = 9;
         double luku2 = 3;
 
-        //tulostetaan muuttujien arvo
+        //Luodaan tulo, jako, summa ja erotus laskutoimitus muuttujat
+        double tulo = luku1 * luku2;
+        double jako = luku1 / luku2;
+        double summa = luku1 + luku2;
+        double erotus = luku1 - luku2;
+
+        //tulostetaan tervehdys ja tekijan nimi
+        System.out.println("Hei olen tulostin ohjelma");
+        System.out.println("Ohjelman tekijä: " + tekija);
+
+        //tulostetaan luku muuttujien arvot
         System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
 
         System.out.println("Luku2-muuttujan arvo on " + luku2 + ".");
 
-        //Luodaan tulo muuttuja
-        double tulo = luku1 * luku2;
-
-        //tulostetaan luku muuttujat ja tulo muuttuja
-        System.out.println(luku1 + " * " + luku2 + "=" + tulo);
-
-        //Luodaan erotus, summa ja jako muuttujat
-        double erotus = luku1 - luku2;
-        
-        double summa = luku1 + luku2;
-
-        double jako = luku1 / luku2;
-
         //tulostetaan lasku toimitukset muuttujien avulla
-        System.out.println(luku1 + " / " + luku2 + "=" + jako);
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
 
-        System.out.println(luku1 + " + " + luku2 + "=" + summa);
+        System.out.println(luku1 + " / " + luku2 + " = " + jako);
 
-        System.out.println(luku1 + " - " + luku2 + "=" + erotus);
+        System.out.println(luku1 + " + " + luku2 + " = " + summa);
+
+        System.out.println(luku1 + " - " + luku2 + " = " + erotus);
     }
 }
