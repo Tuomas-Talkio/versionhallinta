5b49024bd45709746700cc6530f83c86fbead22b
Tuomas-Talkio <122274744+Tuomas-Talkio@users.noreply.github.com>
2025-10-20 14:44:27 +0300

    aloitusteksti
    Vaihdoin oletus "Hello World" viestin tervehdykseksi ohjelmaan. Loin muuttajan jossa on minun nimi nimeltä tekijä ja tulostin "Ohjelman tekijä:" yhdellä riville ja samalla riville tekija muuttujan

--- src/Tulostin.java
+++ src/Tulostin.java
@@ -1,5 +1,11 @@
 public class Tulostin {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("Hei olen tulostin ohjelma");
+        System.out.print("Ohjelman tekijä: ");
+
+        String tekija = "Tuomas Talkio";
+
+        System.out.println(tekija);
     }
 }
