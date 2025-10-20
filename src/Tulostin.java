ff4cbd23c70de02b91ea1eed021f1f8a634c7196
Tuomas-Talkio <122274744+Tuomas-Talkio@users.noreply.github.com>
2025-10-20 14:51:06 +0300

    luku muuttujat
    luodaan kaksi luku muuttujaa ja tulostetaan ne

--- src/Tulostin.java
+++ src/Tulostin.java
@@ -1,11 +1,21 @@
 public class Tulostin {
     public static void main(String[] args) throws Exception {
 
+        //tulostetaan tervehdys
         System.out.println("Hei olen tulostin ohjelma");
         System.out.print("Ohjelman tekijä: ");
 
+        //luodaan String muuttuja ja tulostetaan se
         String tekija = "Tuomas Talkio";
 
         System.out.println(tekija);
+        
+        //Luodaan kaksi int muuttujaa
+        int luku1 = 3;
+        int luku2 = 7;
+
+        System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
+
+        System.out.println("Luku2-muuttujan arvo on " + luku2 + ".");
     }
 }
